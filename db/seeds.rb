d1b80c97b33dbd2353c69e249155d44264dbb7ae
Preston Lee <preston.lee@prestonlee.com>
2016-04-28 16:24:46 -0700

    Deploying alpha environment.

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,15 +15,15 @@ live = Provider.create_with(
 live.reconfigure
 live.save!
 
-smart = Provider.create_with(
-    name: 'SMART HIT',
-    client_id: 'bda1236a-7288-4ca7-991d-fb7f419fea30',
-    client_secret: 'QR-EVIOifG_hoCZsjMKnreXPs486c0-_m-3Yjv5BgsGl_x0zO9wDIoku7gv0stkTk9UaFwqE2mX_TEVokXy0Ww',
-    alternate_client_id: 'eyJhbGciOiJSUzI1NiJ9.eyJhdWQiOlsiYmRhMTIzNmEtNzI4OC00Y2E3LTk5MWQtZmI3ZjQxOWZlYTMwIl0sImlzcyI6Imh0dHBzOlwvXC9hdXRob3JpemUtZHN0dTIuc21hcnRoZWFsdGhpdC5vcmdcLyIsImp0aSI6ImYzZTY0NzI2LTZmMGUtNDQ4Yi1hZjhlLWJmMzg2MGVkNjdiZCIsImlhdCI6MTQ2MTc5NzQ5Nn0.OpaESmBnrK1pmYuWTP9hBSkTTGxaK4OtdynqXkeg_9xV9zMebJH6tx8YbucxGA4Fp3d1UUFJD8o8O0rO-jhXrlBxEVyyhsNiff5OgkqmzPwJW9ynj3EHJ7NRwb_VCLXG4lF_9dlcJvFCpWwVdW1rYXIMeN7R4BXhmBzozy0Lq28'
-# ).find_or_create_by(issuer: 'https://authorize-dstu2.smarthealthit.org')
-).find_or_create_by(issuer: 'https://authorize.smarthealthit.org')
-smart.reconfigure
-smart.save!
+# smart = Provider.create_with(
+#     name: 'SMART HIT',
+#     client_id: 'bda1236a-7288-4ca7-991d-fb7f419fea30',
+#     client_secret: 'QR-EVIOifG_hoCZsjMKnreXPs486c0-_m-3Yjv5BgsGl_x0zO9wDIoku7gv0stkTk9UaFwqE2mX_TEVokXy0Ww',
+#     alternate_client_id: 'eyJhbGciOiJSUzI1NiJ9.eyJhdWQiOlsiYmRhMTIzNmEtNzI4OC00Y2E3LTk5MWQtZmI3ZjQxOWZlYTMwIl0sImlzcyI6Imh0dHBzOlwvXC9hdXRob3JpemUtZHN0dTIuc21hcnRoZWFsdGhpdC5vcmdcLyIsImp0aSI6ImYzZTY0NzI2LTZmMGUtNDQ4Yi1hZjhlLWJmMzg2MGVkNjdiZCIsImlhdCI6MTQ2MTc5NzQ5Nn0.OpaESmBnrK1pmYuWTP9hBSkTTGxaK4OtdynqXkeg_9xV9zMebJH6tx8YbucxGA4Fp3d1UUFJD8o8O0rO-jhXrlBxEVyyhsNiff5OgkqmzPwJW9ynj3EHJ7NRwb_VCLXG4lF_9dlcJvFCpWwVdW1rYXIMeN7R4BXhmBzozy0Lq28'
+# # ).find_or_create_by(issuer: 'https://authorize-dstu2.smarthealthit.org')
+# ).find_or_create_by(issuer: 'https://authorize.smarthealthit.org')
+# smart.reconfigure
+# smart.save!
 
 # admin = User.create!(external_id: 42,
 #                               name: 'Default Administrator',
