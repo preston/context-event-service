dc3cfc540a544ce1c51137d70283b36e672104e6
Preston Lee <preston.lee@prestonlee.com>
2018-05-30 17:55:58 -0700

    Emitting prune completion event.

--- lib/context/agent/pruner.rb
+++ lib/context/agent/pruner.rb
@@ -3,30 +3,36 @@
 module Context
   module Agent
     class Pruner < Base
+      PRUNER_AGENT_URI = 'artaka://agents/pruner'
+      PRUNER_MODEL_URI = 'artaka://agents/pruner'
+      PRUNER_ACTION_URI = 'artaka://agents/pruner/actions/complete'
       def initialize
         super [
-          # Context::Clock::TICK_URI_MINUTE
-          Context::Clock::TICK_URI_SECOND
+          Context::Clock::TICK_URI_MINUTE
+          # Context::Clock::TICK_URI_SECOND
         ]
       end
 
       def run
-        super do |_channel, msg|
+        super do |_channel, _msg|
           # byebug
           ttl = Context::Clock::TICK_TIME_TO_LIVE
           puts "Purging events older than #{ttl}."
           # sleep 3
-          # trigger = JSON.parse(msg)
-					Event.purge_older_than(ttl.ago,
-						Context::Clock::TICK_URI_SECOND, 
-						Context::Clock::TICK_URI_MINUTE, 
-						Context::Clock::TICK_URI_HOUR, 
-						Context::Clock::TICK_URI_DAY
-					)
-
-          #   puts e.to_json
-          # puts "Smith emitting message: #{e.to_json}"
-          # RedisPublisher.new(session_uri_for(e.session_id), e.to_json).process
+          trigger = JSON.parse(_msg)
+          Event.purge_older_than(ttl.ago,
+                                 Context::Clock::TICK_URI_SECOND,
+                                 Context::Clock::TICK_URI_MINUTE,
+                                 Context::Clock::TICK_URI_HOUR,
+                                 Context::Clock::TICK_URI_DAY)
+          e = Event.new(
+            parent_id: trigger['id'],
+            topic_uri: PRUNER_AGENT_URI,
+            agent_uri: PRUNER_AGENT_URI,
+            action_uri: PRUNER_ACTION_URI,
+            model_uri: PRUNER_MODEL_URI
+          )
+          e.save!
         end
       end
     end
